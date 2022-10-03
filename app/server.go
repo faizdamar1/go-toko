003b921da56124edb6b8cd8253148393778e00ff
faizdamar1 <faizdamar123@gmail.com>
2022-10-03 11:44:48 +0700

    connection database

--- app/server.go
+++ app/server.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
@@ -14,8 +17,35 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) Inisialize() {
-	fmt.Println("welcome to gotoko")
+type AppConfig struct {
+	AppName string
+	AppEnv  string
+	AppPort string
+}
+
+type DBConfig struct {
+	DBHost     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	DBPort     string
+	DBDriver   string
+}
+
+func (server *Server) Inisialize(appConfig AppConfig, dbConfig DBConfig) {
+	fmt.Println("welcome to " + appConfig.AppName)
+
+	var err error
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbConfig.DBUser, dbConfig.DBPassword, dbConfig.DBHost, dbConfig.DBPort, dbConfig.DBName)
+
+	server.DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Panic(err.Error())
+	} else {
+		fmt.Println("Connected to DB")
+	}
+
 	server.Router = mux.NewRouter()
 
 	server.inisializeRoutes()
@@ -26,9 +56,35 @@ func (server *Server) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
+
 func Run() {
 	var server = Server{}
+	var appConfig = AppConfig{}
+	var dbConfig = DBConfig{}
 
-	server.Inisialize()
-	server.Run(":9000")
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	appConfig.AppName = getEnv("APP_NAME", "GoTokoApp")
+	appConfig.AppEnv = getEnv("APP_ENV", "development")
+	appConfig.AppPort = getEnv("APP_PORT", "9000")
+
+	dbConfig.DBDriver = getEnv("DB_HOST", "localhost")
+	dbConfig.DBName = getEnv("DB_NAME", "dbname")
+	dbConfig.DBUser = getEnv("DB_USER", "dbuser")
+	dbConfig.DBPassword = getEnv("DB_PASS", "")
+	dbConfig.DBPort = getEnv("DB_PORT", "3306")
+
+	server.Inisialize(appConfig, dbConfig)
+	server.Run(":" + appConfig.AppPort)
 }
